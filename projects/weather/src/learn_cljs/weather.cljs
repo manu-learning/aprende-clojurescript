6c12535b01d5d72875cead5c7ab1e4b79bd08983
Emmanuel <emmanuel.lazarte.012@gmail.com>
2022-10-30 16:45:49 -0300

    Añadido: Proyecto de prueba Weather con figwheel

--- projects/weather/src/learn_cljs/weather.cljs
+++ projects/weather/src/learn_cljs/weather.cljs
@@ -1,39 +1,78 @@
-(ns ^:figwheel-hooks learn-cljs.weather
+(ns ^:figwheel-hooks learn-cljs.weather                    ;; <1>
   (:require
    [goog.dom :as gdom]
-   [reagent.core :as reagent :refer [atom]]
-   [reagent.dom :as rdom]))
+   [reagent.dom :as rdom]
+   [reagent.core :as r]))
 
-(println "This text is printed from src/learn_cljs/weather.cljs. Go ahead and edit it and see reloading in action.")
-
-(defn multiply [a b] (* a b))
-
-;; define your app data so that it doesn't get over-written on reload
-(defonce app-state (atom {:text "Hello world!"}))
-
-(defn get-app-element []
-  (gdom/getElement "app"))
-
-(defn hello-world []
+(defn hello-world []                                       ;; <2>
   [:div
-   [:h1 (:text @app-state)]
-   [:h3 "Edit this in src/learn_cljs/weather.cljs and watch it change!"]])
+   [:h1 {:class "app-title"} "Hello, World"]])
 
-(defn mount [el]
-  (rdom/render [hello-world] el))
+(defonce app-state (r/atom {:title "WhichWeather"
+                            :postal-code ""
+                            :temperatures {:today {:label "Today"
+                                                   :value nil}
+                                           :tomorrow {:label "Tomorrow"
+                                                      :value nil}}}))
 
-(defn mount-app-element []
-  (when-let [el (get-app-element)]
-    (mount el)))
+;; XPath: //h1
+(defn title []
+  [:h1 (:title @app-state)])
 
-;; conditionally start your application based on the presence of an "app" element
-;; this is particularly helpful for testing this ns without launching the app
+;; XPath: //div[@class='temperature']/div[@class='value'] | //div[@class='temperature']/h2
+(defn temperature [temp]
+  [:div {:class "temperature"}
+   [:div {:class "value"}
+    (:value temp)]
+   [:h2 (:label temp)]])
+
+;; .-target obtiene el elemento input del DOM
+;; .-value obtiene el valor del atributo value del input (input que previamente capturamos con .-target)
+
+;; reescribo la escucha del evento on-change de diferentes formas para que se entienda mejor como funciona
+(comment
+  "si usamos la función anónima acortada"
+  #(swap! app-state assoc :postal-code (.-value (.-target %)))
+
+  "si usamos la función anónima acortada y la macro arrow"
+  #(swap! app-state assoc :postal-code (-> % .-target .-value))
+
+  "si usamos la función anónima"
+  (fn [x]
+    (swap! app-state assoc
+           :postal-code (.-value (.-target x))))
+
+  "si usamos la función anónima y la macro arrow"
+  (fn [x]
+    (swap! app-state assoc
+           :postal-code (-> x .-target .-value))))
+(def actualizar (fn [x]
+  (swap! app-state assoc
+         :postal-code (-> x .-target .-value))))
+
+;; XPath: //div[@class='postal-code'][h3|input|button]
+(defn postal-code []
+  [:div {:class "postal-code"}
+   [:h3 "Escribi tu código postal"]
+   [:input {:type "text"
+            :placeholder "Código Postal"
+            :value (:postal-code @app-state)
+            :on-change #(swap! app-state assoc :postal-code (-> % .-target .-value))}]
+   [:button "Ir"]])
+
+;; XPath: //div[@class='app']/div[@class='temperatures']
+(defn app []
+  [:div {:class "app"}
+   [title]
+   [:div {:class "temperatures"}
+    (for [temp (vals (:temperatures @app-state))]
+      [temperature temp])]
+   [postal-code]])
+
+(defn mount-app-element []                                 ;; <3>
+  (rdom/render [app] (gdom/getElement "app")))
+
 (mount-app-element)
 
-;; specify reload hook with ^:after-load metadata
-(defn ^:after-load on-reload []
-  (mount-app-element)
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+(defn ^:after-load on-reload []                            ;; <4>
+  (mount-app-element))
