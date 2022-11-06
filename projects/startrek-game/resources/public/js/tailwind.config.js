f8121e86d866fd0985ad3dbd7662f3f1694afae9
Emmanuel <emmanuel.lazarte.012@gmail.com>
2022-11-06 18:58:59 -0300

    Añadido: Configuración de Tailwindcss

---
+++ projects/startrek-game/resources/public/js/tailwind.config.js
@@ -0,0 +1,10 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            animation: {
+                'blink': 'blink 1s steps(1) infinite',
+                'typing': 'typing 4s steps(38) 1s 1 normal both, blink 1s steps(1) infinite'
+            }
+        }
+    }
+}
