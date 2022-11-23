e89784f9ffa4331a9f93413d98b5931befb94aa9
Emmanuel <emmanuel.lazarte.012@gmail.com>
2022-11-23 14:24:17 -0300

    Añadido: projecto-interoperabilidad-cljs-js, transformando código cljs a js y viceversa

---
+++ projects/interoperability-with-js/resources/public/js/scoresFixture.js
@@ -0,0 +1,21 @@
+var players = [
+    {id:1, score:100, category: "chess"},
+    {id:2, score:200, category: "chess"},
+    {id:5, score:300, category: "chess"},
+    {id:9, score:900, category: "chess"},
+];
+
+/*
+myfixtures = function(){
+    var ranking = {};
+
+    ranking.scores = [
+        {id:1, score:100, category: "chess"},
+        {id:2, score:200, category: "chess"},
+        {id:5, score:300, category: "chess"},
+        {id:9, score:900, category: "chess"},
+    ];
+
+    return ranking;
+};
+*/
--- projects/interoperability-with-js/src/learn_cljs/interoperability_with_js.cljs
+++ projects/interoperability-with-js/src/learn_cljs/interoperability_with_js.cljs
@@ -4,20 +4,84 @@
    [reagent.core :as reagent :refer [atom]]
    [reagent.dom :as rdom]))
 
-(println "This text is printed from src/learn_cljs/interoperability_with_js.cljs. Go ahead and edit it and see reloading in action.")
+;; TODO: en la REPL cambiar el namespace por default con siguiente.. (ns learn-cljs.interoperability-with-js)
 
-(defn multiply [a b] (* a b))
-
-;; define your app data so that it doesn't get over-written on reload
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-state (atom {:text "Interoperabilidad con JavaScript!"}))
 
 (defn get-app-element []
   (gdom/getElement "app"))
 
+;; definimos en clojure un vector con estructuras map como elementos
+(def personajes [{:id 1 :nombre "Hisoka"}
+                 {:id 2 :nombre "Gon"}
+                 {:id 9 :nombre "Killua"}
+                 {:id 15 :nombre "Kurapika"}])
+
+;; clj->js
+;; - transformamos código Clojure a un objeto JavaScript
+(def personajes-js (clj->js personajes))
+
+;; #js
+;; - definimos en javascript un array
+(def examen-cazador #js ["Fase Preliminar"
+                          "Primera Fase"
+                          "Un juego a Medianoche"
+                          "Tercera Fase"
+                          "Cuarta Fase"
+                          "Fase Final"])
+
+;; aget
+;; - obtenemos un elemento de un array de javascript
+(def primera-fase-examen-cazador
+  (aget examen-cazador 0))
+
+;; .nombreFuncionJavascript
+;; - usamos métodos/funciones primitivas de javascript con arrays (de javascript)
+(def numero-de-etapa-final-examen-cazador
+  (.indexOf examen-cazador "Fase Final"))
+
+;; aset
+;; - generamos efecto de lado sobre un array de javascript, cambiando el valor de un elemento,
+;; - si una variable está bindeada/vinculada a un objeto javascript, entonces puede mutar (por tanto no es inmutable..)
+(defn renombrar-fase-examen [numero-fase nuevo-nombre]
+  (aset examen-cazador numero-fase nuevo-nombre))
+
+;; #js
+;; - definimos un objeto en JavaScript
+(def presidente-cazadores
+  #js {"id" 1
+       "nombre" "Isaac Netero",
+       "rango" "Presidente de Asociación de Cazadores",
+       "edad" 110,
+       "habilidades" ["fuerza superhumana"
+                      "velocidad"
+                      "resistencia"],
+       "nen" {"tipo" "intensificación",
+              "poderes" ["Primera Mano"
+                         "Tercera Mano"
+                         "100-Tipo de Guanyin"]}})
+
+;; .-nombreAtributo
+;; - obtenemos el valor asociado a un atributo de un objeto javascript con .-
+(def edad-presidente-cazadores
+  (.-edad presidente-cazadores))
+
+;; js->clj
+;; - transformamos un objeto javascript a clojure, para usar las operaciones propias de clojure
+;; - se suele sugerir esto para aprovechar la inmutabilidad característica de Clojure
+(def habilidades-presidente-cazadores
+  (map
+   (fn [habilidad] (str "habilidad: " habilidad))
+   (js->clj (.-habilidades presidente-cazadores))))
+
+;; TODO: no funciona, devuelve nil
+(println
+ (.. presidente-cazadores -nen -tipo))
+
 (defn hello-world []
   [:div
    [:h1 (:text @app-state)]
-   [:h3 "in src/learn_cljs/interoperability_with_js.cljs and watch it change!"]])
+   [:h3 "Probando la interoperabilidad entre ClojureScript y JavaScript"]])
 
 (defn mount [el]
   (rdom/render [hello-world] el))
@@ -26,14 +90,7 @@
   (when-let [el (get-app-element)]
     (mount el)))
 
-;; conditionally start your application based on the presence of an "app" element
-;; this is particularly helpful for testing this ns without launching the app
 (mount-app-element)
 
-;; specify reload hook with ^:after-load metadata
 (defn ^:after-load on-reload []
-  (mount-app-element)
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+  (mount-app-element))
